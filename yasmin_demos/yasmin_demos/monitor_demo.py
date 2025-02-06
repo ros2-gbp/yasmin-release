8f564b1342d3a23465d8d6fca9301426715dc24e
Miguel Ángel González Santamarta <mgons@unileon.es>
2025-02-06 08:21:43 +0000

    Rebase from 'upstream'

--- yasmin_demos/yasmin_demos/monitor_demo.py
+++ yasmin_demos/yasmin_demos/monitor_demo.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright (C) 2023  Miguel Ángel González Santamarta
+# Copyright (C) 2023 Miguel Ángel González Santamarta
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -23,7 +23,7 @@ import yasmin
 from yasmin import Blackboard
 from yasmin import StateMachine
 from yasmin_ros import MonitorState
-from yasmin_ros.ros_logs import set_ros_loggers
+from yasmin_ros import set_ros_loggers
 from yasmin_ros.basic_outcomes import TIMEOUT
 from yasmin_viewer import YasminViewerPub
 

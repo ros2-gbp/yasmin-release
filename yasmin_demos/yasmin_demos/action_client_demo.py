ec2fd884b4a50ad1577298a9f8be91fbb56520bc
Miguel Ángel González Santamarta <mgons@unileon.es>
2025-02-06 08:26:13 +0000

    Rebase from 'upstream'

--- yasmin_demos/yasmin_demos/action_client_demo.py
+++ yasmin_demos/yasmin_demos/action_client_demo.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright (C) 2023  Miguel Ángel González Santamarta
+# Copyright (C) 2023 Miguel Ángel González Santamarta
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -21,7 +21,7 @@ from example_interfaces.action import Fibonacci
 import yasmin
 from yasmin import CbState, Blackboard, StateMachine
 from yasmin_ros import ActionState
-from yasmin_ros.ros_logs import set_ros_loggers
+from yasmin_ros import set_ros_loggers
 from yasmin_ros.basic_outcomes import SUCCEED, ABORT, CANCEL
 from yasmin_viewer import YasminViewerPub
 

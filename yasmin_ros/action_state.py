7474bf776b7fd868818d2550926bfcc67d6d4b42
Miguel Ángel González Santamarta <mgons@unileon.es>
2025-10-12 16:31:30 +0200

    Rebase from 'debian/rolling/yasmin_ros'

--- yasmin_ros/action_state.py
+++ yasmin_ros/action_state.py
@@ -13,8 +13,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from typing import Set, Callable, Type, Any
 from threading import RLock, Event
+from typing import Set, Callable, Type, Any
 
 from rclpy.node import Node
 from rclpy.task import Future
@@ -28,28 +28,32 @@ from yasmin import State
 from yasmin import Blackboard
 from yasmin_ros.yasmin_node import YasminNode
 from yasmin_ros.basic_outcomes import SUCCEED, ABORT, CANCEL, TIMEOUT
+from yasmin_ros.ros_communications_cache import ROSCommunicationsCache
 
 
 class ActionState(State):
     """
-    Represents a state that interacts with a ROS 2 action server.
+    A state class for handling ROS 2 action client operations.
 
-    This class encapsulates the functionality needed to manage
-    the sending and receiving of goals to and from an action server.
+    This class encapsulates the behavior of a ROS 2 action client within a YASMIN
+    state. It allows the creation and management of goals, feedback, and results
+    associated with an action server.
 
     Attributes:
-        _node (Node): The ROS 2 node instance used to communicate with the action server.
-        _action_name (str): The name of the action to be performed.
-        _action_client (ActionClient): The action client used to send goals.
+        _node (Node): Shared pointer to the ROS 2 node.
+        _action_name (str): Name of the action to communicate with.
+        _action_client (ActionClient): Shared pointer to the action client.
         _action_done_event (Event): Event used to wait for action completion.
-        _action_result (Any): The result returned by the action server.
-        _action_status (GoalStatus): The status of the action execution.
-        _goal_handle (ClientGoalHandle): Handle for the goal sent to the action server.
+        _action_result (Any): Shared pointer to the action result.
+        _action_status (GoalStatus): Status of the action execution.
+        _goal_handle (ClientGoalHandle): Handle for the current goal.
         _goal_handle_lock (RLock): Lock to manage access to the goal handle.
-        _create_goal_handler (Callable[[Blackboard], Any]): Function that creates the goal to send.
-        _result_handler (Callable[[Blackboard, Any], str]): Function to handle the result from the action server.
-        _feedback_handler (Callable[[Blackboard, Any], None]): Function to handle feedback from the action server.
-        _timeout (float): Timeout duration for waiting for the action server.
+        _create_goal_handler (Callable[[Blackboard], Any]): Handler function for creating goals.
+        _result_handler (Callable[[Blackboard, Any], str]): Handler function for processing results.
+        _feedback_handler (Callable[[Blackboard, Any], None]): Handler function for processing feedback.
+        _wait_timeout (float): Maximum time to wait for the action server.
+        _response_timeout (float): Timeout for the action response.
+        _maximum_retry (int): Maximum number of retries.
     """
 
     def __init__(
@@ -62,23 +66,28 @@ class ActionState(State):
         feedback_handler: Callable = None,
         callback_group: CallbackGroup = None,
         node: Node = None,
-        timeout: float = None,
+        wait_timeout: float = None,
+        response_timeout: float = None,
+        maximum_retry: int = 3,
     ) -> None:
         """
-        Initializes the ActionState instance.
+        Construct an ActionState with a specific action name and goal handler.
 
-        This constructor sets up the action client and prepares to handle goals.
+        This constructor initializes the action state with a specified action name,
+        goal handler, and optional timeout.
 
-        Parameters:
+        Args:
             action_type (Type): The type of the action to be executed.
-            action_name (str): The name of the action to be executed.
-            create_goal_handler (Callable[[Blackboard], Any])): A function that generates the goal.
-            outcomes (Set[str], optional): Additional outcomes that this state can return.
-            result_handler (Callable[[Blackboard, Any], str], optional): A function to process the result of the action.
-            feedback_handler (Callable[[Blackboard, Any], None], optional): A function to process feedback from the action.
+            action_name (str): The name of the action to communicate with.
+            create_goal_handler (Callable[[Blackboard], Any]): A function that creates a goal for the action.
+            outcomes (Set[str], optional): A set of possible outcomes for this action state.
+            result_handler (Callable[[Blackboard, Any], str], optional): A function to handle the result of the action.
+            feedback_handler (Callable[[Blackboard, Any], None], optional): A function to handle feedback from the action.
             callback_group (CallbackGroup, optional): The callback group for the action client.
             node (Node, optional): The ROS 2 node to use. If None, uses the default YasminNode.
-            timeout (float, optional): Timeout duration for waiting for the action server.
+            wait_timeout (float, optional): The maximum time to wait for the action server. Default is None (wait indefinitely).
+            response_timeout (float, optional): The maximum time to wait for the action response. Default is None (wait indefinitely).
+            maximum_retry (int, optional): Maximum retries of the action if it returns timeout. Default is 3.
 
         Raises:
             ValueError: If create_goal_handler is None.
@@ -86,48 +95,58 @@ class ActionState(State):
 
         ## Event used to wait for action completion.
         self._action_done_event: Event = Event()
-        ## The result returned by the action server.
+        ## Shared pointer to the action result.
         self._action_result: Any = None
-        ## The status of the action execution.
+        ## Status of the action execution.
         self._action_status: GoalStatus = None
-        ## Handle for the goal sent to the action server.
+        ## Handle for the current goal.
         self._goal_handle: ClientGoalHandle = None
         ## Lock to manage access to the goal handle.
         self._goal_handle_lock: RLock = RLock()
 
-        ## Function that creates the goal to send.
+        ## Handler function for creating goals.
         self._create_goal_handler: Callable[[Blackboard], Any] = create_goal_handler
-        ## Function to handle the result from the action server.
+        ## Handler function for processing results.
         self._result_handler: Callable[[Blackboard, Any], str] = result_handler
-        ## Function to handle feedback from the action server.
+        ## Handler function for processing feedback.
         self._feedback_handler: Callable[[Blackboard, Any], None] = feedback_handler
 
-        ## Timeout duration for waiting for the action server.
-        self._timeout: float = timeout
+        ## Maximum time to wait for the action server.
+        self._wait_timeout: float = wait_timeout
+        ## Timeout for the action response.
+        self._response_timeout: float = response_timeout
+
+        ## Maximum number of retries.
+        self._maximum_retry: int = maximum_retry
 
         _outcomes = [SUCCEED, ABORT, CANCEL]
 
-        if self._timeout:
+        if self._wait_timeout or self._response_timeout:
             _outcomes.append(TIMEOUT)
 
         if outcomes:
             _outcomes = _outcomes + outcomes
 
-        ## The ROS 2 node instance used to communicate with the action server.
+        ## Shared pointer to the ROS 2 node.
         self._node: Node = node
 
         if self._node is None:
             self._node: Node = YasminNode.get_instance()
 
-        ## The name of the action to be performed.
+        ## Name of the action to communicate with.
         self._action_name: str = action_name
 
-        ## The action client used to send goals.
-        self._action_client: ActionClient = ActionClient(
-            self._node,
-            action_type,
-            action_name,
-            callback_group=callback_group,
+        ## Action type for caching
+        self._action_type: Type = action_type
+
+        ## Shared pointer to the action client (reused from cache if available).
+        self._action_client: ActionClient = (
+            ROSCommunicationsCache.get_or_create_action_client(
+                self._node,
+                action_type,
+                action_name,
+                callback_group,
+            )
         )
 
         if not self._create_goal_handler:
@@ -137,42 +156,47 @@ class ActionState(State):
 
     def cancel_state(self) -> None:
         """
-        Cancels the current action state.
+        Cancel the current action state.
 
-        This method cancels the goal sent to the action server, if it exists.
+        This function cancels the goal sent to the action server, if it exists.
         """
         with self._goal_handle_lock:
             if self._goal_handle is not None:
                 self._goal_handle.cancel_goal()
-
         super().cancel_state()
 
     def execute(self, blackboard: Blackboard) -> str:
         """
-        Executes the action state by sending a goal to the action server.
+        Execute the action and return the outcome.
 
-        This method waits for the action server to be available, sends the goal,
-        and waits for the action to complete, handling feedback and results.
+        This function creates a goal using the provided goal handler, sends the
+        goal to the action server, and waits for the result or feedback.
 
-        Parameters:
-            blackboard (Blackboard): The blackboard instance used for state management.
+        Args:
+            blackboard (Blackboard): A shared pointer to the blackboard used for communication.
 
         Returns:
-            str: The outcome of the action execution (e.g., SUCCEED, ABORT, CANCEL, TIMEOUT).
-
-        Raises:
-            Exception: Raises an exception if any error occurs during action execution.
+            str: A string representing the outcome of the action execution.
+                Possible outcomes include SUCCEED, ABORT, CANCEL, or TIMEOUT.
         """
         goal = self._create_goal_handler(blackboard)
+        retry_count = 0
 
         yasmin.YASMIN_LOG_INFO(f"Waiting for action '{self._action_name}'")
-        act_available = self._action_client.wait_for_server(self._timeout)
 
-        if not act_available:
+        while not self._action_client.wait_for_server(self._wait_timeout):
             yasmin.YASMIN_LOG_WARN(
                 f"Timeout reached, action '{self._action_name}' is not available"
             )
-            return TIMEOUT
+
+            if self._maximum_retry > 0:
+                if retry_count < self._maximum_retry:
+                    retry_count += 1
+                    yasmin.YASMIN_LOG_WARN(
+                        f"Retrying to connect to action '{self._action_name}' ({retry_count}/{self._maximum_retry})"
+                    )
+                else:
+                    return TIMEOUT
 
         self._action_done_event.clear()
 
@@ -187,8 +211,23 @@ class ActionState(State):
         )
         send_goal_future.add_done_callback(self._goal_response_callback)
 
+        if self.is_canceled():
+            return CANCEL
+
         # Wait for action to be done
-        self._action_done_event.wait()
+        while not self._action_done_event.wait(self._response_timeout):
+            yasmin.YASMIN_LOG_WARN(
+                f"Timeout reached while waiting for response from action '{self._action_name}'"
+            )
+
+            if retry_count < self._maximum_retry:
+                retry_count += 1
+                yasmin.YASMIN_LOG_WARN(
+                    f"Retrying to wait for action '{self._action_name}' response ({retry_count}/{self._maximum_retry})"
+                )
+            else:
+                return TIMEOUT
+
         status = self._action_status
 
         if status == GoalStatus.STATUS_CANCELED:
@@ -207,25 +246,26 @@ class ActionState(State):
 
     def _goal_response_callback(self, future: Future) -> None:
         """
-        Callback to handle the response from sending a goal.
+        Callback for handling the goal response.
 
         This method retrieves the goal handle and sets up the result callback.
 
-        Parameters:
+        Args:
             future: The future object representing the result of the goal sending operation.
         """
+
         with self._goal_handle_lock:
             self._goal_handle: ClientGoalHandle = future.result()
-            get_result_future = self._goal_handle.get_result_async()
+            get_result_future: Future = self._goal_handle.get_result_async()
             get_result_future.add_done_callback(self._get_result_callback)
 
     def _get_result_callback(self, future: Future) -> None:
         """
-        Callback to handle the result of the executed action.
+        Callback for handling the result of the executed action.
 
         This method sets the action result and status, and signals that the action is done.
 
-        Parameters:
+        Args:
             future: The future object representing the result of the action execution.
         """
         self._action_result: Any = future.result().result

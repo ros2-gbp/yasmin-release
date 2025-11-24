547c0255d96417e48f8da5e2810605c186cada18
Miguel Ángel González Santamarta <mgons@unileon.es>
2025-11-24 13:28:42 +0100

    Rebase from 'release/jazzy/yasmin_demos'

--- src/factory_demo.cpp
+++ src/factory_demo.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 Pedro Edom Nunes
+// Copyright (C) 2025 Miguel Ángel González Santamarta
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -13,48 +13,49 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+#include <iostream>
+#include <memory>
 #include <string>
 
+#include "ament_index_cpp/get_package_share_directory.hpp"
 #include "rclcpp/rclcpp.hpp"
-#include "yasmin/logs.hpp"
-#include "yasmin/state.hpp"
 #include "yasmin/state_machine.hpp"
-#include "yasmin_demos/bar_state.h"
-#include "yasmin_demos/foo_state.h"
+#include "yasmin_factory/yasmin_factory.hpp"
 #include "yasmin_ros/ros_logs.hpp"
 #include "yasmin_viewer/yasmin_viewer_pub.hpp"
 
 int main(int argc, char *argv[]) {
-  YASMIN_LOG_INFO("yasmin_multiple_states_demo");
+  YASMIN_LOG_INFO("yasmin_factory_demo");
   rclcpp::init(argc, argv);
 
-  // Set ROS 2 logs
+  // Set up ROS 2 loggers
   yasmin_ros::set_ros_loggers();
 
-  // Create a state machine
-  auto sm = std::make_shared<yasmin::StateMachine>(
-      std::initializer_list<std::string>{"outcome4"});
+  std::string outcome;
+
+  // Create the factory and state machine in a scope to ensure proper cleanup
+  yasmin_factory::YasminFactory factory;
+
+  // Load state machine from XML file
+  std::string xml_file =
+      ament_index_cpp::get_package_share_directory("yasmin_demos") +
+      "/state_machines/demo_2.xml";
+
+  // Create the state machine from the XML file
+  auto sm = factory.create_sm_from_file(xml_file);
 
   // Cancel state machine on ROS 2 shutdown
-  rclcpp::on_shutdown([sm]() {
-    if (sm->is_running()) {
-      sm->cancel_state();
+  std::weak_ptr<yasmin::StateMachine> weak_sm = sm;
+  rclcpp::on_shutdown([weak_sm]() {
+    if (auto sm = weak_sm.lock()) {
+      if (sm->is_running()) {
+        sm->cancel_state();
+      }
     }
   });
 
-  // Add states to the state machine
-  sm->add_state("FOO", std::make_shared<FooState>(),
-                {
-                    {"outcome1", "BAR"},
-                    {"outcome2", "outcome4"},
-                });
-  sm->add_state("BAR", std::make_shared<BarState>(),
-                {
-                    {"outcome3", "FOO"},
-                });
-
   // Publisher for visualizing the state machine
-  yasmin_viewer::YasminViewerPub yasmin_pub(sm, "YASMIN_MULTIPLE_STATES_DEMO");
+  yasmin_viewer::YasminViewerPub yasmin_pub(sm, "YASMIN_FACTORY_DEMO");
 
   // Execute the state machine
   try {
@@ -64,7 +65,7 @@ int main(int argc, char *argv[]) {
     YASMIN_LOG_WARN(e.what());
   }
 
+  // Shutdown ROS 2
   rclcpp::shutdown();
-
   return 0;
 }

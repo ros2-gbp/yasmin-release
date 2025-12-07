153f5ddb653fc4ed221369fb21c1b3998ac18c9b
Miguel Ángel González Santamarta <mgons@unileon.es>
2025-12-07 11:39:27 +0100

    Rebase from 'upstream'

--- yasmin_demos/src/parameters_demo.cpp
+++ yasmin_demos/src/parameters_demo.cpp
@@ -112,14 +112,7 @@ int main(int argc, char *argv[]) {
 
   // Create a state machine
   auto sm = std::make_shared<yasmin::StateMachine>(
-      std::initializer_list<std::string>{"outcome4"});
-
-  // Cancel state machine on ROS 2 shutdown
-  rclcpp::on_shutdown([sm]() {
-    if (sm->is_running()) {
-      sm->cancel_state();
-    }
-  });
+      std::initializer_list<std::string>{"outcome4"}, true);
 
   // Add states to the state machine
   sm->add_state("GETTING_PARAMETERS",

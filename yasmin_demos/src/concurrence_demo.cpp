58f7e5374711720ec06c599178cb04eeeeb26e43
Miguel Ángel González Santamarta <mgons@unileon.es>
2025-12-07 10:56:20 +0000

    Rebase from 'upstream'

--- yasmin_demos/src/concurrence_demo.cpp
+++ yasmin_demos/src/concurrence_demo.cpp
@@ -125,14 +125,7 @@ int main(int argc, char *argv[]) {
 
   // Create a state machine
   auto sm = std::make_shared<yasmin::StateMachine>(
-      std::initializer_list<std::string>{"outcome4"});
-
-  // Cancel state machine on ROS 2 shutdown
-  rclcpp::on_shutdown([sm]() {
-    if (sm->is_running()) {
-      sm->cancel_state();
-    }
-  });
+      std::initializer_list<std::string>{"outcome4"}, true);
 
   // Create states to run concurrently
   auto foo_state = std::make_shared<FooState>();

939db8cc0298b140a599aa0e1cd6b834c60dcb8b
Miguel Ángel González Santamarta <mgons@unileon.es>
2026-04-13 13:43:59 +0000

    Revert "Trimmed the branch to only the yasmin_factory sub directory"
    This reverts commit 72c1b1a355e3583e6fca175cfd667bf7bffeff2f.

---
+++ yasmin_demos/yasmin_demos/monitor_demo.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2023 Miguel Ángel González Santamarta
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import rclpy
+from rclpy.qos import qos_profile_sensor_data
+from nav_msgs.msg import Odometry
+
+import yasmin
+from yasmin import Blackboard, StateMachine
+from yasmin_ros import MonitorState
+from yasmin_ros import set_ros_loggers
+from yasmin_ros.basic_outcomes import TIMEOUT, CANCEL
+from yasmin_viewer import YasminViewerPub
+
+
+class PrintOdometryState(MonitorState):
+    """
+    MonitorState subclass to handle Odometry messages.
+
+    This state monitors Odometry messages from the specified ROS topic,
+    logging them and transitioning based on the number of messages received.
+    """
+
+    def __init__(self) -> None:
+        """
+        Initializes the PrintOdometryState.
+        """
+        super().__init__(
+            Odometry,  # msg type
+            "odom",  # topic name
+            ["outcome1", "outcome2"],  # outcomes
+            self.monitor_handler,  # monitor handler callback
+            qos=qos_profile_sensor_data,  # qos for the topic subscription
+            msg_queue=10,  # queue for the monitor handler callback
+            timeout=10,  # timeout to wait for messages in seconds
+        )
+        self.times = 5
+
+    def monitor_handler(self, blackboard: Blackboard, msg: Odometry) -> str:
+        """
+        Handles incoming Odometry messages.
+
+        This method is called whenever a new Odometry message is received.
+        It logs the message, decrements the count of messages to process,
+        and determines the next state outcome.
+
+        Args:
+            blackboard (Blackboard): The shared data storage for states.
+            msg (Odometry): The incoming Odometry message.
+
+        Returns:
+            str: The next state outcome, either "outcome1" to continue
+                 monitoring or "outcome2" to transition to the next state.
+
+        Exceptions:
+            None
+        """
+        yasmin.YASMIN_LOG_INFO(msg)
+
+        self.times -= 1
+
+        if self.times <= 0:
+            return "outcome2"
+
+        return "outcome1"
+
+
+def main() -> None:
+    # Initialize ROS 2
+    rclpy.init()
+
+    # Set ROS 2 logs
+    set_ros_loggers()
+    yasmin.YASMIN_LOG_INFO("yasmin_monitor_demo")
+
+    # Create a finite state machine (FSM)
+    sm = StateMachine(outcomes=["outcome4"], handle_sigint=True)
+
+    # Add states to the FSM
+    sm.add_state(
+        "PRINTING_ODOM",
+        PrintOdometryState(),
+        transitions={
+            "outcome1": "PRINTING_ODOM",
+            "outcome2": "outcome4",
+            TIMEOUT: "outcome4",
+            CANCEL: "outcome4",
+        },
+    )
+
+    # Publish FSM information for visualization
+    YasminViewerPub(sm, "YASMIN_MONITOR_DEMO")
+
+    # Execute the FSM
+    try:
+        outcome = sm()
+        yasmin.YASMIN_LOG_INFO(outcome)
+    except Exception as e:
+        yasmin.YASMIN_LOG_WARN(e)
+
+    # Shutdown ROS 2 if it's running
+    if rclpy.ok():
+        rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()

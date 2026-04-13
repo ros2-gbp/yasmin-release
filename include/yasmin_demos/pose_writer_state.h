1c6dfb946bddbe97a37849309597ceb2487a1820
Miguel Ángel González Santamarta <mgons@unileon.es>
2026-04-13 13:58:15 +0000

    Rebase from 'debian/kilted/yasmin_demos'

--- include/yasmin_demos/pose_writer_state.h
+++ include/yasmin_demos/pose_writer_state.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 Pedro Edom Nunes
+// Copyright (C) 2026 Maik Knof
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -13,32 +13,33 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+#ifndef YASMIN_DEMOS_POSE_WRITER_STATE_H
+#define YASMIN_DEMOS_POSE_WRITER_STATE_H
+
 #include <string>
 
 #include <yasmin/blackboard.hpp>
 #include <yasmin/state.hpp>
 #include <yasmin/types.hpp>
 
-#ifndef YASMIN_DEMOS_FOO_STATE_H
-#define YASMIN_DEMOS_FOO_STATE_H
-
 /**
- * @brief Represents the "Foo" state in the state machine.
+ * @brief Represents a state that creates and serializes a Pose message.
  *
- * This state increments a counter each time it is executed and
- * communicates the current count via the blackboard.
+ * This state creates a geometry_msgs::msg::Pose message, serializes it to a
+ * byte array, and stores both the serialized data and the type string in the
+ * blackboard.
  */
-class FooState : public yasmin::State {
+class PoseWriterState : public yasmin::State {
 public:
   /**
-   * @brief Constructs a FooState object, initializing the counter.
+   * @brief Constructs a PoseWriterState object.
    */
-  FooState();
+  PoseWriterState();
 
   /**
-   * @brief Destructs the FooState object.
+   * @brief Destructs the PoseWriterState object.
    */
-  ~FooState();
+  ~PoseWriterState();
 
   /**
    * @brief Configures the state-local parameters.
@@ -46,25 +47,22 @@ public:
   void configure() override;
 
   /**
-   * @brief Executes the Foo state logic.
+   * @brief Executes the Pose writer state logic.
    *
-   * This method logs the execution, waits for the configured duration,
-   * increments the counter, and sets a string in the blackboard.
-   * The state will transition to either "outcome1" or "outcome2"
-   * based on the current value of the counter.
+   * This method creates a Pose message, serializes it, stores the serialized
+   * bytes in the blackboard under "pose_bytes", and stores the type string
+   * under "pose_bytes__type".
    *
    * @param blackboard Shared pointer to the blackboard for state communication.
-   * @return std::string The outcome of the execution: "outcome1" or "outcome2".
+   * @return std::string The outcome of the execution: "outcome1".
    */
   std::string execute(yasmin::Blackboard::SharedPtr blackboard);
 
-  /// Counter to track the number of executions.
-  int counter;
-
 private:
-  std::string counter_prefix_;
-  int max_count_;
-  int sleep_ms_;
+  double position_x_;
+  double position_y_;
+  double position_z_;
+  double orientation_w_;
 };
 
-#endif // YASMIN_DEMOS_FOO_STATE_H
+#endif // YASMIN_DEMOS_POSE_WRITER_STATE_H

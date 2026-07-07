884d357de48c6acf2ce4f1bdaa490730008f133a
Miguel Ángel González Santamarta <mgons@unileon.es>
2026-07-07 17:33:44 +0200

    Rebase from 'upstream'

--- yasmin_ros/yasmin_ros/publisher_state.py
+++ yasmin_ros/yasmin_ros/publisher_state.py
@@ -1,17 +1,16 @@
 # Copyright (C) 2025 Miguel Ángel González Santamarta
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from typing import Callable, Union, Type, Any
 
@@ -22,7 +21,7 @@ from rclpy.callback_groups import CallbackGroup
 
 import yasmin
 from yasmin import State, Blackboard
-from yasmin_ros.yasmin_node import YasminNode
+from yasmin_ros.ros_state_utils import resolve_node
 from yasmin_ros.basic_outcomes import SUCCEED
 from yasmin_ros.ros_clients_cache import ROSClientsCache
 
@@ -59,11 +58,7 @@ class PublisherState(State):
         ## Callback handler to create messages.
         self._create_message_handler: Callable[[Blackboard], Any] = create_message_handler
 
-        ## Shared pointer to the ROS 2 node.
-        self._node: Node = node
-
-        if self._node is None:
-            self._node = YasminNode.get_instance()
+        self._node = resolve_node(node)
 
         ## Name of the topic to publish to.
         self._topic_name: str = topic_name

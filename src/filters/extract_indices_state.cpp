0baea9b6dd78be9b23b61d80e0bf1fadced18ea8
Miguel Ángel González Santamarta <mgons@unileon.es>
2026-07-07 17:47:51 +0200

    Rebase from 'release/lyrical/yasmin_pcl'

--- src/filters/extract_indices_state.cpp
+++ src/filters/extract_indices_state.cpp
@@ -1,17 +1,16 @@
 // Copyright (C) 2026 Maik Knof
 //
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include "yasmin_pcl/filters/extract_indices_state.hpp"
 
@@ -64,10 +63,10 @@ namespace yasmin_pcl::filters {
 
 ExtractIndicesState::ExtractIndicesState()
     : yasmin::State({"succeeded", "aborted"}) {
-  negative_ = false;
-  keep_organized_ = false;
-  user_filter_value_ = std::numeric_limits<float>::quiet_NaN();
-  extract_removed_indices_ = false;
+  this->negative_ = false;
+  this->keep_organized_ = false;
+  this->user_filter_value_ = std::numeric_limits<float>::quiet_NaN();
+  this->extract_removed_indices_ = false;
 
   this->set_description(
       "Applies pcl::ExtractIndices to a pcl::PCLPointCloud2 stored in the "
@@ -105,13 +104,11 @@ ExtractIndicesState::ExtractIndicesState()
                        "Removed point indices stored as pcl::Indices.");
 }
 
-ExtractIndicesState::~ExtractIndicesState() {}
-
 void ExtractIndicesState::configure() {
-  negative_ = this->get_parameter<bool>("negative");
-  keep_organized_ = this->get_parameter<bool>("keep_organized");
-  user_filter_value_ = this->get_parameter<float>("user_filter_value");
-  extract_removed_indices_ =
+  this->negative_ = this->get_parameter<bool>("negative");
+  this->keep_organized_ = this->get_parameter<bool>("keep_organized");
+  this->user_filter_value_ = this->get_parameter<float>("user_filter_value");
+  this->extract_removed_indices_ =
       this->get_parameter<bool>("extract_removed_indices");
 }
 
@@ -133,9 +130,9 @@ ExtractIndicesState::execute(yasmin::Blackboard::SharedPtr blackboard) {
 
     pcl::ExtractIndices<pcl::PCLPointCloud2> filter;
     filter.setInputCloud(input_cloud);
-    filter.setNegative(negative_);
-    filter.setKeepOrganized(keep_organized_);
-    filter.setUserFilterValue(user_filter_value_);
+    filter.setNegative(this->negative_);
+    filter.setKeepOrganized(this->keep_organized_);
+    filter.setUserFilterValue(this->user_filter_value_);
     common::set_optional_input_indices(filter, blackboard);
 
     auto output_cloud = common::make_pcl_point_cloud2();
@@ -146,7 +143,7 @@ ExtractIndicesState::execute(yasmin::Blackboard::SharedPtr blackboard) {
     filter.filter(output_indices);
     blackboard->set<common::Indices>("output_indices", output_indices);
 
-    if (extract_removed_indices_) {
+    if (this->extract_removed_indices_) {
       const auto domain_indices = make_domain_indices(input_cloud);
       blackboard->set<common::Indices>(
           "removed_indices",

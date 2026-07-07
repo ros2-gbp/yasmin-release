185d17e01b5f0c64442d6b4799325b9b5aff614b
Miguel Ángel González Santamarta <mgons@unileon.es>
2026-07-07 17:40:25 +0200

    Rebase from 'release/rolling/yasmin_pcl'

--- include/yasmin_pcl/io/save_ply_state.hpp
+++ include/yasmin_pcl/io/save_ply_state.hpp
@@ -1,17 +1,16 @@
 // Copyright (C) 2026 Maik Knof
 //
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #ifndef YASMIN_PCL__IO__SAVE_PLY_STATE_HPP_
 #define YASMIN_PCL__IO__SAVE_PLY_STATE_HPP_
@@ -31,23 +30,42 @@ namespace yasmin_pcl::io {
  */
 class SavePlyState : public yasmin::State {
 public:
+  /** @brief Construct a SavePlyState. */
   SavePlyState();
-  ~SavePlyState() override;
+  /** @brief Default destructor. */
+  ~SavePlyState() override = default;
 
+  /** @brief Configure from blackboard parameters. */
   void configure() override;
+  /**
+   * @brief Save a point cloud to a PLY file.
+   * @param blackboard The shared blackboard.
+   * @return Outcome string.
+   */
   std::string execute(yasmin::Blackboard::SharedPtr blackboard) override;
 
 private:
+  /// @brief Path to the PLY file to save
   std::string file_path_;
+  /// @brief Whether to write in binary mode
   bool binary_mode_;
+  /// @brief Whether to use camera coordinates
   bool use_camera_;
+  /// @brief Origin X coordinate
   float origin_x_;
+  /// @brief Origin Y coordinate
   float origin_y_;
+  /// @brief Origin Z coordinate
   float origin_z_;
+  /// @brief Origin W component
   float origin_w_;
+  /// @brief Orientation X component
   float orientation_x_;
+  /// @brief Orientation Y component
   float orientation_y_;
+  /// @brief Orientation Z component
   float orientation_z_;
+  /// @brief Orientation W component
   float orientation_w_;
 };
 

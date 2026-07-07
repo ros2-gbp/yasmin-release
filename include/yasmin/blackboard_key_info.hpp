3769840cdccc24c6367f9a843d3175fe71627dd9
Miguel Ángel González Santamarta <mgons@unileon.es>
2026-07-07 17:12:16 +0200

    Rebase from 'release/humble/yasmin'

--- include/yasmin/blackboard_key_info.hpp
+++ include/yasmin/blackboard_key_info.hpp
@@ -1,17 +1,16 @@
 // Copyright (C) 2026 Miguel Ángel González Santamarta
 //
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #ifndef YASMIN__BLACKBOARD_KEY_INFO_HPP_
 #define YASMIN__BLACKBOARD_KEY_INFO_HPP_
@@ -82,7 +81,7 @@ struct BlackboardKeyInfo {
         default_value_type(demangle_type(typeid(std::decay_t<T>).name())) {
     using ValueType = std::decay_t<T>;
     auto stored = std::static_pointer_cast<ValueType>(this->default_value);
-    inject_default = [stored](Blackboard &bb, const std::string &key) {
+    this->inject_default = [stored](Blackboard &bb, const std::string &key) {
       bb.set<ValueType>(key, *stored);
     };
   }
@@ -100,7 +99,7 @@ struct BlackboardKeyInfo {
         has_default(true), default_value(std::make_shared<std::string>(value)),
         default_value_type(demangle_type(typeid(std::string).name())) {
     auto stored = std::static_pointer_cast<std::string>(this->default_value);
-    inject_default = [stored](Blackboard &bb, const std::string &key) {
+    this->inject_default = [stored](Blackboard &bb, const std::string &key) {
       bb.set<std::string>(key, *stored);
     };
   }

a11574f9638dc1de77cb3318eae28274c052b2a0
Miguel Ángel González Santamarta <mgons@unileon.es>
2026-07-07 17:10:39 +0200

    Imported upstream version '6.0.0' of 'upstream'

--- yasmin_factory/test/test_yasmin_factory.py
+++ yasmin_factory/test/test_yasmin_factory.py
@@ -1,24 +1,23 @@
 # Copyright (C) 2025 Miguel Ángel González Santamarta
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import unittest
 import os
 import tempfile
 from lxml import etree as ET
 from yasmin import Blackboard, StateMachine
-from yasmin_factory.yasmin_factory import YasminFactory
+from yasmin_factory.factory import YasminFactory
 
 
 class TestYasminFactory(unittest.TestCase):
@@ -418,6 +417,15 @@ class TestYasminFactory(unittest.TestCase):
         self.assertEqual(sm.get_parameter("int_dict"), {"a": 1, "b": 2})
         self.assertEqual(sm.get_parameter("float_dict"), {"a": 1.0, "b": 2.5})
         self.assertEqual(sm.get_parameter("bool_dict"), {"a": True, "b": False})
+
+    def test_create_orthogonal_state(self):
+        """Test creating and executing an orthogonal state from XML."""
+        xml_path = os.path.join(self.test_dir, "test_orthogonal_sm.xml")
+        sm = self.factory.create_sm_from_file(xml_path)
+        self.assertIsNotNone(sm)
+        bb = Blackboard()
+        outcome = sm(bb)
+        self.assertEqual(outcome, "done")
 
 
 if __name__ == "__main__":
--- yasmin_plugins_manager/setup.py
+++ yasmin_plugins_manager/setup.py
@@ -4,7 +4,7 @@ package_name = "yasmin_plugins_manager"
 
 setup(
     name=package_name,
-    version="5.0.0",
+    version="6.0.0",
     packages=[package_name],
     data_files=[
         ("share/ament_index/resource_index/packages", [f"resource/{package_name}"]),
@@ -15,7 +15,7 @@ setup(
     maintainer="Maik Knof",
     maintainer_email="maik.knof@gmx.de",
     description="Plugin discovery and caching for YASMIN states.",
-    license="GPL-3.0",
+    license="Apache-2.0",
     extras_require={
         "test": [
             "pytest",

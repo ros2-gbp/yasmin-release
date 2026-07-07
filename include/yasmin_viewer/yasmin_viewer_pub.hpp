0a508f4f5386a3a0ad77932d3d70b4da4cd5b760
Miguel Ángel González Santamarta <mgons@unileon.es>
2026-07-07 17:26:34 +0200

    Rebase from 'debian/jazzy/yasmin_viewer'

--- include/yasmin_viewer/yasmin_viewer_pub.hpp
+++ include/yasmin_viewer/yasmin_viewer_pub.hpp
@@ -1,29 +1,29 @@
 // Copyright (C) 2023 Miguel Ángel González Santamarta
 //
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #ifndef YASMIN_VIEWER__YASMIN_VIEWER_PUB_HPP_
 #define YASMIN_VIEWER__YASMIN_VIEWER_PUB_HPP_
 
-#include <map>
 #include <memory>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 #include <rclcpp/rclcpp.hpp>
 
 #include "yasmin/concurrence.hpp"
+#include "yasmin/orthogonal_state.hpp"
 #include "yasmin/state.hpp"
 #include "yasmin/state_machine.hpp"
 #include "yasmin/types.hpp"
@@ -44,8 +44,8 @@ public:
    * @brief Constructs YasminViewerPub with a given ROS 2 node, state machine
    * name, and state machine instance.
    * @param node Shared pointer to the ROS 2 node.
-   * @param fsm_name Name of the finite state machine.
    * @param fsm Shared pointer to the StateMachine instance to be published.
+   * @param fsm_name Name of the finite state machine.
    */
   YasminViewerPub(const rclcpp::Node::SharedPtr &node,
                   yasmin::StateMachine::SharedPtr fsm,
@@ -54,15 +54,15 @@ public:
   /**
    * @brief Constructs YasminViewerPub with a default ROS 2 node instance, state
    * machine name, and state machine instance.
-   * @param fsm_name Name of the finite state machine.
    * @param fsm Shared pointer to the StateMachine instance to be published.
+   * @param fsm_name Name of the finite state machine.
    */
   YasminViewerPub(yasmin::StateMachine::SharedPtr fsm,
                   const std::string &fsm_name);
 
   /**
-   * @brief Constructs YasminViewerPub with a given ROS 2 node, state machine
-   * name, and state machine instance.
+   * @brief Constructs YasminViewerPub with a given ROS 2 node and state
+   * machine.
    * @param node Shared pointer to the ROS 2 node.
    * @param fsm Shared pointer to the StateMachine instance to be published.
    */
@@ -70,8 +70,8 @@ public:
                   yasmin::StateMachine::SharedPtr fsm);
 
   /**
-   * @brief Constructs YasminViewerPub with a default ROS 2 node instance, state
-   * machine name, and state machine instance.
+   * @brief Constructs YasminViewerPub with a default ROS 2 node instance and
+   * state machine.
    * @param fsm Shared pointer to the StateMachine instance to be published.
    */
   YasminViewerPub(yasmin::StateMachine::SharedPtr fsm);
@@ -92,7 +92,7 @@ public:
    * @param concurrence Shared pointer to the Concurrence state.
    * @return Map of state names to their transition vectors.
    */
-  std::map<std::string, std::vector<yasmin_msgs::msg::Transition>>
+  std::unordered_map<std::string, std::vector<yasmin_msgs::msg::Transition>>
   parse_concurrence_transitions(
       yasmin::Concurrence::SharedPtr concurrence) const;
 

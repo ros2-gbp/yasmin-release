c2c65b6d8f6323730123aa9d7d23c2ea90548e2c
Miguel Ángel González Santamarta <mgons@unileon.es>
2026-07-26 11:26:31 +0000

    Rebase from 'release/lyrical/yasmin_viewer'

--- web/app.js
+++ web/app.js
@@ -1,18 +1,17 @@
 /*
 Copyright (C) 2026 Maik Knof
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+    http://www.apache.org/licenses/LICENSE-2.0
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 
 const state = {
@@ -182,11 +181,23 @@ function cloneNodes(rawStates, fsmName, fsmIndex) {
 }
 
 function computeActiveFlags(nodesById) {
+  const parentsWithAllActive = [];
+
   nodesById.forEach((node) => {
-    if (nodesById.has(node.current_state)) {
+    if (node.current_state === -2) {
+      parentsWithAllActive.push(node.id);
+    } else if (nodesById.has(node.current_state)) {
       nodesById.get(node.current_state).active = true;
     }
   });
+
+  if (parentsWithAllActive.length > 0) {
+    nodesById.forEach((node) => {
+      if (parentsWithAllActive.includes(node.parent)) {
+        node.active = true;
+      }
+    });
+  }
 
   nodesById.forEach((node) => {
     if (!node.active) {
@@ -579,7 +590,8 @@ function getViewportContentOffset(viewport, zoomWrapper) {
   }
 
   const { width, height } = getViewportContentSize(zoomWrapper);
-  const { width: innerWidth, height: innerHeight } = getViewportInnerSize(viewport);
+  const { width: innerWidth, height: innerHeight } =
+    getViewportInnerSize(viewport);
   const zoom = getZoom(zoomWrapper);
 
   return {
@@ -599,7 +611,8 @@ function centerViewport(viewport, zoomWrapper) {
   applyViewportContentOffset(viewport, zoomWrapper);
 
   const { width, height } = getViewportContentSize(zoomWrapper);
-  const { width: innerWidth, height: innerHeight } = getViewportInnerSize(viewport);
+  const { width: innerWidth, height: innerHeight } =
+    getViewportInnerSize(viewport);
   const zoom = getZoom(zoomWrapper);
   const offset = getViewportContentOffset(viewport, zoomWrapper);
 
@@ -628,7 +641,8 @@ function resetViewportZoom(viewport, zoomWrapper) {
 
 function fitViewport(viewport, zoomWrapper) {
   const { width, height } = getViewportContentSize(zoomWrapper);
-  const { width: innerWidth, height: innerHeight } = getViewportInnerSize(viewport);
+  const { width: innerWidth, height: innerHeight } =
+    getViewportInnerSize(viewport);
 
   if (width <= 0 || height <= 0 || innerWidth <= 0 || innerHeight <= 0) {
     return;
@@ -686,8 +700,7 @@ function zoomAroundPoint(viewport, zoomWrapper, nextZoom, clientX, clientY) {
 
   const offsetX =
     clientX - rect.left + viewport.scrollLeft - previousOffset.left;
-  const offsetY =
-    clientY - rect.top + viewport.scrollTop - previousOffset.top;
+  const offsetY = clientY - rect.top + viewport.scrollTop - previousOffset.top;
 
   const contentX = offsetX / previousZoom;
   const contentY = offsetY / previousZoom;
